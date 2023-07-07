a85c1ad06928f96353b6ae535ac5523f5ed4a434
C0A21037 <85731650+C0A21037@users.noreply.github.com>
2023-07-07 15:43:59 +0900

    Add files via upload

---
+++ sensin_projitu/WEB-INF/src/pnw/PnwServletFilter.java
@@ -0,0 +1,40 @@
+package pnw;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = { "/*" })
+public class PnwServletFilter implements Filter {
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        req.setCharacterEncoding("UTF-8");
+        res.setContentType("text/html; charset=UTF-8");
+        
+
+        chain.doFilter(req, res);
+
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
